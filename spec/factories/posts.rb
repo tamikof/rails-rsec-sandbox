85adc1ce704f293c9a5d0c55d625de3291b77d7d
tamiko funabiki <tf081981@gmail.com>
2025-01-25 01:22:14 +0900

    FactoryにStatusとIsAdminのtraitを追加した

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -5,5 +5,25 @@ FactoryBot.define do
     tag { "" }
     status { Post.statuses.values.sample }
     is_admin { Post.is_admins.values.sample }
+
+    trait :draft do
+      status { :draft }
+    end
+
+    trait :published do
+      status { :published }
+    end
+
+    trait :archived do
+      status { :archived }
+    end
+
+    trait :admin do
+      is_admin { :admin }
+    end
+
+    trait :general do
+      is_admin { :general }
+    end
   end
 end
